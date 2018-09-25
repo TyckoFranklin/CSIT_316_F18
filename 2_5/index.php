e3978b68088c3d62b52424fcd1f3f4330d4f5b25
Tycko Franklin <Tycko.franklin@students.columbiabasin.edu>
2018-09-24 20:33:35 -0700

    Added the correct (ish) creation time stamps.

--- 2_5/index.php
+++ 2_5/index.php
@@ -3,7 +3,7 @@
  * Created by PhpStorm.
  * User: TyckoFranklin
  * Date: 9/23/2018
- * Time: 10:17 AM
+ * Time: 10:38 AM
  */
 print("<!DOCTYPE html  PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>");
 print("<html lang='en'>");
